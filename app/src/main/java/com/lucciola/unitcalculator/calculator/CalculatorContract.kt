a34c039a1fb7fe7d236a95a6e512f52c1deadef5
Lucciola <hidenari.t1997nov@gmail.com>
2018-08-23 12:44:53 +0900

    add: contract

--- app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorContract.kt
+++ app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorContract.kt
@@ -1,2 +1,22 @@
 package com.lucciola.unitcalculator.calculator
 
+import com.lucciola.unitcalculator.BasePresenter
+import com.lucciola.unitcalculator.BaseView
+
+interface CalculatorContract {
+
+    interface View : BaseView<Presenter> {
+        fun showResult(result: String)
+        fun showFormula(formula: String)
+    }
+
+    interface Presenter : BasePresenter {
+        fun onEvaluate()
+        fun onInputNumber(number: String)
+        fun onInputOperator(operator: String)
+        fun onInputBrace(brace: String)
+        fun onInputUnit(unit: String)
+        fun onInputPrefix(prefix: String)
+    }
+
+}
