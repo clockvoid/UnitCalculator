d8ea73a3ec4ea4eb43a86b88b94d5554b3d913da
Lucciola <hidenari.t1997nov@gmail.com>
2018-08-23 12:46:44 +0900

    implement: fragment and presenter

--- app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorFragment.kt
+++ app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorFragment.kt
@@ -1,2 +1,39 @@
 package com.lucciola.unitcalculator.calculator
 
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.lucciola.unitcalculator.R
+import kotlinx.android.synthetic.main.calculator_fragment.view.*
+
+class CalculatorFragment : CalculatorContract.View, Fragment() {
+
+    override lateinit var presenter: CalculatorContract.Presenter
+    private var mFormulaTextView: TextView? = null
+    private var mResultTextView: TextView? = null
+
+    companion object {
+        fun newInstance(): CalculatorFragment = CalculatorFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val root: View? = inflater.inflate(R.layout.calculator_fragment, container, false)
+
+        this.mFormulaTextView = root?.formulaTextView
+        this.mResultTextView = root?.resultTextView
+
+        return root ?: inflater.let { super.onCreateView(it, container, savedInstanceState) }
+    }
+
+    override fun showResult(result: String) {
+        this.mResultTextView?.text = result
+    }
+
+    override fun showFormula(formula: String) {
+        this.mFormulaTextView?.text = formula
+    }
+
+}
--- app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorPresenter.kt
+++ app/src/main/java/com/lucciola/unitcalculator/calculator/CalculatorPresenter.kt
@@ -1,2 +1,54 @@
 package com.lucciola.unitcalculator.calculator
 
+import FormulaParser.Parser
+import Exception.RuntimeErrorException
+import Exception.SyntaxErrorException
+
+class CalculatorPresenter(calculatorView: CalculatorContract.View) : CalculatorContract.Presenter {
+
+    private val mCalculatorView: CalculatorContract.View = calculatorView
+    private var mFormula: String = ""
+    private val mParser = Parser("")
+
+    init {
+        mCalculatorView.presenter = this
+    }
+
+    override fun start() {
+    }
+
+    override fun onEvaluate() {
+        this.mParser.setProgram(mFormula)
+        val ans: String = try {
+            mParser.compile().evaluate()
+        } catch(e: RuntimeErrorException) {
+            "runtime error : " + e.message
+        } catch(e: SyntaxErrorException) {
+            "syntax error : " + e.message
+        }
+        mCalculatorView.showResult(ans)
+    }
+
+    override fun onInputNumber(number: String) {
+        this.mFormula += number
+        this.mCalculatorView.showFormula(this.mFormula)
+    }
+
+    override fun onInputOperator(operator: String) {
+        this.mFormula += " $operator "
+        this.mCalculatorView.showFormula(this.mFormula)
+    }
+
+    override fun onInputBrace(brace: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInputUnit(unit: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onInputPrefix(prefix: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
